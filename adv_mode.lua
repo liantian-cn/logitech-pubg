a8d31b72fc67b70bd082e30d4c11c184802170ed
liantian <liantian.me+code@gmail.com>
2017-10-10 20:43:47 +0800

    fix

--- adv_mode.lua
+++ adv_mode.lua
@@ -1,40 +1,23 @@
 local recoil_table = {}
 recoil_table["m4"] = {
-    base={7.8,7.8,7.8,7.8,7.8,7.8,7.8,13.8,13.8,13.8,13.8,9.8,9.8,9.8,9.8,9.8,10,10,10,10.1,10.1,10.1},
-    x4={26,26,26,26,26,26,26,45,45,45,45,29,29,29,29,29,29,29,29,29,29,29},
-    aim_speed = 86,
-    vert = 7, 
-    vert_speed=15,
-    vert_recovery= 9.5
+    base={5,5,5,5,5,5,5,7,7,7,7,7,7,7,7,7,7,6,6,6,6,6},
+    x4={26,26,26,26,26,26,26,45,45,45,45,29,29,29,29,29,29,29,29,29,29,29}
 }
 recoil_table["m16"] = {
     base={7,7,7,7,7,7,7,13.6,13.6,13.6,13.6,10.2,10.2,10.2,10.2,10.2,10.2,10.2,10.2,8,8,8},
-    x4={26,26,26,26,26,26,26,45,45,45,45,29,29,29,29,29,29,29,29,29,29,29},
-    aim_speed = 75,
-    vert = 7, 
-    vert_speed=11,
-    vert_recovery= 9.5
+    x4={26,26,26,26,26,26,26,45,45,45,45,29,29,29,29,29,29,29,29,29,29,29}
 }
 recoil_table["ak"] = {
     base={7.1,7.1,7.1,7.1,7.1,7.1,7.1,7.1,7.1,7.1,9,9,9,9,8.9,8.9,8.9,8.9,8.9,8.9,8.9},
-    x4={20,20,20,20,20,20,20,20,20,20,37,37,37,37,28,28,28,28,28,28,28},
-    aim_speed = 100,
-    vert = 8, 
-    vert_speed=15,
-    vert_recovery= 9.5
+    x4={20,20,20,20,20,20,20,20,20,20,37,37,37,37,28,28,28,28,28,28,28}
 }
 recoil_table["ump9"] = {
     base={5.5,5.5,5.5,5.5,5.5,5.5,5.5,6.9,6.9,6.9,6.9,7.1,7.1,7.1,7.1,7.1,7.1,7.1,7.1,7,7,7},
-    x4={25,25,25,25,25,25,25,35,35,35,35,28,28,28,28,28,28,28,28,28,28,28},
-    aim_speed = 92,
-    vert = 5, 
-    vert_speed=15,
-    vert_recovery= 5
+    x4={25,25,25,25,25,25,25,35,35,35,35,28,28,28,28,28,28,28,28,28,28,28}
 }
-
-local speed_revise = 5
 -------------------------------------------------------------------------------------------------------
 local weapon = "none"
+local click_enbale = false
 
 -------------------------------------------------------------------------------------------------------
 function recoil_value(_weapon,_mode,_shotTime)
@@ -54,7 +37,7 @@ function recoil_mode()
     if IsKeyLockOn("capslock") then
       return "x4"
     else
-      return "base"
+        return "base"
     end
 end 
 
@@ -65,6 +48,7 @@ function OnEvent(event, arg)
         EnablePrimaryMouseButtonEvents(true)
     elseif event == "PROFILE_DEACTIVATED" then
         weapon = "none"
+        click_enbale = false
         ReleaseKey("Left")
         ReleaseMouseButton(1)
     end
@@ -72,9 +56,9 @@ function OnEvent(event, arg)
     if (event == "MOUSE_BUTTON_PRESSED" and arg == 6) then
         -- button 6 : Disable
         weapon = "none"
-    elseif (event == "MOUSE_BUTTON_PRESSED" and arg == 4) then
-        -- button 4 : AKM
-        weapon = "ak"
+ --   elseif (event == "MOUSE_BUTTON_PRESSED" and arg == 4) then
+ --       -- button 4 : AKM
+ --      weapon = "ak"
     elseif (event == "MOUSE_BUTTON_PRESSED" and arg == 5) then
         -- button 5 : M16A4
         weapon = "m16"
@@ -86,14 +70,16 @@ function OnEvent(event, arg)
         weapon = "ump9"
     elseif (event == "MOUSE_BUTTON_PRESSED" and arg == 1) then
         -- button 1 : Shoot  
+        -- 循环
         shotTime = 0.0
         repeat
-        -- 循环
-            seed = 1.0 + math.random()
+            -- seed = (1.0 + math.random())/2
+            seed = 1.0
             shotTime = shotTime + 30 * seed
             mode = recoil_mode()
             step = recoil_value(weapon,mode,shotTime)
-
+            OutputLogMessage("seed = %s\n", seed)
+            OutputLogMessage("step = %s\n", step)
             PressAndReleaseKey("Left")
             Sleep(30 * seed)
             MoveMouseRelative(0, step * seed)
